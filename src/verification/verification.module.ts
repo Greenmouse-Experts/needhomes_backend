98d705b8f554053687d13b34cce7e74cc807f08e
chidi <tyler.igwe@gmail.com>
2026-01-28 07:27:21 +0100

    feat(verification): add PartnerModule import to VerificationModule

--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { VerificationController } from './verification.controller';
 import { VerificationService } from './verification.service';
 import { VerificationRepository } from './verification.repository';
+import { PartnerModule } from 'src/partner/partner.module';
 
 @Module({
+  imports: [PartnerModule],
   controllers: [VerificationController],
   providers: [VerificationService, VerificationRepository],
   exports: [VerificationService, VerificationRepository],
